e92dfb6c62484c4a850193b7c9656fbe5a88da65
Thomas <Thomas60808@gmail.com>
2022-12-24 22:34:28 +0800

    login page rendered

---
+++ final/backend/src/server.js
@@ -0,0 +1,32 @@
+import { GraphQLServer, PubSub } from 'graphql-yoga';
+import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
+import Subscription from './resolvers/Subscription';
+
+import { createPubSub, createSchema, createYoga } from 'graphql-yoga';
+import { createServer } from 'node:http';
+import * as fs from 'fs';
+
+const pubsub = createPubSub();
+
+const yoga = createYoga({
+  schema: createSchema({
+    typeDefs: fs.readFileSync(
+      './src/schema.graphql',
+      'utf-8' ),
+    resolvers: {
+      Query,
+      Mutation,
+      Subscription
+    },
+  }),
+  context: {
+    pubsub, 
+  },
+  graphqlEndpoint: '/'
+});
+
+const server = createServer(yoga);
+
+export default server;
+
